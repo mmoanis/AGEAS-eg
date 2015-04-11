3456f67e6cd36907e27e8eb2210d0c572ee24b74
mohamed-moanis <mohamed_moanis@outlook.com>
2015-04-11 19:34:00 +0200

    formating

--- databaseScript.sql
+++ databaseScript.sql
@@ -2,9 +2,9 @@
 --
 USE master ;
 
+-- remove datbase if it already exists
 IF EXISTS(select * from sys.databases where name='AGEAS')
-
-DROP DATABASE AGEAS
+    DROP DATABASE AGEAS
 
 
 GO
@@ -22,45 +22,45 @@ GO
 
 CREATE TABLE Customer ( 
     Customer_ID INT IDENTITY(1, 1) PRIMARY KEY,
-    Name Nvarchar(50) NOT NULL,
-    Balance decimal(3, 3) 
+    Name NVARCHAR(50) NOT NULL,
+    Balance DECIMAL(3, 3) 
 )
 
  -- suppliers table
 
 CREATE TABLE Supplier (
     Supplier_ID INT IDENTITY(1, 1) PRIMARY KEY,
-    Name Nvarchar(50) NOT NULL,
-    Phone varchar(14) NOT NULL UNIQUE,
-    Address nvarchar(100) NOT NULL,
-    Company nvarchar(50) NOT NULL UNIQUE
+    Name NVARCHAR(50) NOT NULL,
+    Phone VARCHAR(14) NOT NULL UNIQUE,
+    Address NVARCHAR(100) NOT NULL,
+    Company NVARCHAR(50) NOT NULL UNIQUE
 )
 
 GO
 
-
+-- product table
 CREATE TABLE Product (
     Product_ID INT IDENTITY(1, 1) PRIMARY KEY,
     Supplier_ID INT
         FOREIGN KEY REFERENCES Supplier(Supplier_ID),
-    Name Nvarchar(50) NOT NULL,
-    TYPE nvarchar(50),
-    Price decimal(3, 3) NOT NULL
+    Name NVARCHAR(50) NOT NULL,
+    TYPE NVARCHAR(50),
+    Price DECIMAL(3, 3) NOT NULL
 )
 
-
+-- purchases table
 CREATE TABLE Purchase (
     Purchase_ID INT IDENTITY(1, 1) PRIMARY KEY,
     Customer_ID INT
         FOREIGN KEY REFERENCES Customer(Customer_ID),
     Name Nvarchar(50) NOT NULL,
-    Discount decimal(3, 3) NOT NULL,
-    Total_Revenue decimal(3, 3) NOT NULL
+    Discount DECIMAL(3, 3) NOT NULL,
+    Total_Revenue DECIMAL(3, 3) NOT NULL
 )
 
-
+-- product purchaese relation
 CREATE TABLE Product_Purchase (
-    Product_Purchase_ID int IDENTITY(1, 1) PRIMARY KEY,
+    Product_Purchase_ID INT IDENTITY(1, 1) PRIMARY KEY,
     Product_ID INT 
         FOREIGN KEY REFERENCES Product(Product_ID),
     Purchase_ID INT
