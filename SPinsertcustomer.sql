2ab0d15c59fd6cef7c0a0bde3af733d9cfbc1c14
mohamed <mohamed_moanis@outlook.com>
2015-04-17 15:48:40 +0200

    removed customer_id
    id is auto generated

--- SPinsertcustomer.sql
+++ SPinsertcustomer.sql
@@ -1,6 +1,5 @@
 CREATE PROCEDURE Insertcustomer
 (
-@Customer_ID INT,
 @Name  NVARCHAR(50) ,
 @Balance  DECIMAL(3,3)
 )
@@ -9,13 +8,11 @@ AS
 BEGIN
 INSERT INTO Customer
 (
-Customer_ID, 
 Name,
  Balance
 ) 
 VALUES
 (
-@Customer_ID,
 @Name,
 @Balance
 )
